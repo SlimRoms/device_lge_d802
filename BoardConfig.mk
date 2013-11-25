155d618ecf142fb9db0ce4f113dcec287ddd4247
Griffin Millender <griffinn.millender@gmail.com>
2013-11-25 06:45:17 -0600

    Change kernel config
    Change-Id: I79cc6b6c105b0c650bec0f6d388debb69b2d2074

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_d802_defconfig
+TARGET_KERNEL_CONFIG := d802_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := d802,g2,galbi,d800
 
